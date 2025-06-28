5740902130d6b0e193c1669f839399c266c25ef9
Harshit525191 <2023eeb1205@iitrpr.ac.in>
2025-06-29 01:23:50 +0530

    first commit

--- vending.v
+++ vending.v
@@ -1,65 +1,65 @@
 module vending (
     input clk,
     input reset,
-    input [1:0] coin, // 01 = 5rs and 10 = 10rs
+    input [1:0] coinn, // 01 = 5rs and 10 = 10rs
     output reg out,
-    output reg[1:0] change
+    output reg[1:0] changee
 );
-parameter s0 = 2'b00; 
-parameter s1 = 2'b01; // 5rs 
-parameter s2 = 2'b10; // 10rs 
-    reg[1:0] state, next_state;
+parameter a0 = 2'b00; 
+parameter a1 = 2'b01; // 5rs 
+parameter a2 = 2'b10; // 10rs 
+    reg[1:0] state, Nextstate;
     always @(posedge clk or reset == 1) begin
         if (reset) begin
-            state = s0;
+            state = a0;
             out = 0;
-            change = 2'b00;
+            changee = 2'b00;
         end else begin
-            state = next_state;
+            state = Nextstate;
         end
         case (state)        
-            s0:
-            if(coin==2'b00) begin
-                next_state = s0; 
+            a0:
+            if(coinn==2'b00) begin
+                Nextstate = a0; 
                 out = 0;
-                change = 2'b00;
-            end else if(coin == 2'b01) begin
-                next_state = s1; 
+                changee = 2'b00;
+            end else if(coinn == 2'b01) begin
+                Nextstate = a1; 
                 out = 0;
-                change = 2'b00;
-            end else if(coin == 2'b10) begin
-                next_state = s2; 
+                changee = 2'b00;
+            end else if(coinn == 2'b10) begin
+                Nextstate = a2; 
                 out = 0;
-                change = 2'b00;
+                changee = 2'b00;
             end
-            s1:
-            if(coin ==2'b00) begin
-                next_state=s1;
+            a1:
+            if(coinn ==2'b00) begin
+                Nextstate=a1;
                 out=0;
-                change=2'b00;
+                changee=2'b00;
             end
-            else if(coin == 2'b01) begin
-                next_state = s2; 
+            else if(coinn == 2'b01) begin
+                Nextstate = a2; 
                 out = 0;
-                change = 2'b00;
-            end else if(coin == 2'b10) begin
-                next_state = s0; 
+                changee = 2'b00;
+            end else if(coinn == 2'b10) begin
+                Nextstate = a0; 
                 out = 1; 
-                change = 2'b00; 
+                changee = 2'b00; 
             end
-            s2:
-            if(coin == 2'b00) begin
-                next_state = s2; 
+            a2:
+            if(coinn == 2'b00) begin
+                Nextstate = a2; 
                 out = 0;
-                change = 2'b00;
-            end else if(coin == 2'b01) begin
-                next_state = s0; 
+                changee = 2'b00;
+            end else if(coinn == 2'b01) begin
+                Nextstate = a0; 
                 out = 1; 
-                change = 2'b00; 
-            end else if(coin == 2'b10) begin
-                next_state = s1; 
+                changee = 2'b00; 
+            end else if(coinn == 2'b10) begin
+                Nextstate = a1; 
                 out = 1; 
-                change = 2'b01; 
+                changee = 2'b01; 
             end
         endcase
     end    
